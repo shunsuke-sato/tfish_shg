e81627b63b3832095b1b9d97f30961eb33c4161e
Shunsuke A. Sato <shunsuke.sato26@gmail.com>
2022-10-11 17:53:18 +0900

    Developping.

--- maxwell.f90
+++ maxwell.f90
@@ -2,7 +2,6 @@ module global_variables
   implicit none
   
 ! math constant
-  complex(8),parameter :: zi = (0d0, 1d0)
   real(8),parameter :: pi = 4d0*atan(1d0) 
 
 ! physics constants
@@ -22,13 +21,12 @@ module global_variables
   
   
 ! electric field
-  real(8),parameter :: omega_IR = 1.55d0*ev, Tpulse_IR = 200d0*fs
+  real(8),parameter :: omega_IR = 1.55d0*ev, Tpulse_IR = 100d0*fs
   real(8),parameter :: omega_THz = 0.00414d0*ev, Tpulse_THz = 2d3*fs
   real(8),parameter :: v_IR = clight/1.00027505d0, v_SHG = clight/1.00028276d0
   real(8),parameter :: v_THz = clight/(1d0+274d-6)
-  real(8),allocatable :: xx(:)
-  complex(8),allocatable :: zE_shg(:)
-  complex(8),allocatable :: zE_shg_o(:),zE_shg_n(:),zG_E_shg(:), zPt(:)
+  real(8),allocatable :: E_shg(:), xx(:)
+  real(8),allocatable :: E_shg_o(:),E_shg_n(:),L_E_shg(:), Pt(:)
 
 
 end module global_variables
@@ -48,9 +46,9 @@ subroutine input
   implicit none
   integer :: ix
   
-  x_left = -1d6*nm
-  x_right = 1.1d6*nm
-  L_matter = 1d6*nm
+  x_left = -1d4*nm
+  x_right = 1.1d8*nm
+  L_matter = 1d8*nm
 
   hx = 40d0*nm
 
@@ -62,13 +60,13 @@ subroutine input
   write(*,*)"refined hx =",hx
 
 
-  allocate(zE_shg(0-2:nx+2))
-  allocate(zE_shg_o(0:nx),zE_shg_n(0:nx))
-  allocate(zG_E_shg(0:nx))
+  allocate(E_shg(0-2:nx+2))
+  allocate(E_shg_o(0:nx),E_shg_n(0:nx))
+  allocate(L_E_shg(0:nx))
 
   allocate(xx(0:nx))
-  zE_shg = 0d0
-  zE_shg_o = 0d0; zE_shg_n = 0d0
+  E_shg = 0d0
+  E_shg_o = 0d0; E_shg_n = 0d0
 
   xx = 0d0
   do ix = 0, nx
@@ -90,7 +88,7 @@ subroutine input
   end do
 
 
-  allocate(zPt(mx_s:mx_e))
+  allocate(Pt(mx_s:mx_e))
 
 end subroutine input
 !-------------------------------------------------------------------------
@@ -122,16 +120,15 @@ subroutine dt_evolve(tt)
 
 
   call calc_Pt(tt)
-  call calc_zG_E_shg
+  call calc_L_E_shg
 
-  zE_shg_n(0:nx) = zE_shg_o(0:nx) -(2d0*dt*V_SHG)*( -zG_E_shg(0:nx) )
+  E_shg_n(0:nx) = 2d0*E_shg(0:nx)-E_shg_o(0:nx) + v_SHG**2*dt**2*L_E_shg(0:nx)
+  E_shg_n(mx_s:mx_e) = E_shg_n(mx_s:mx_e) &
+      - 4d0*pi*(v_shg/clight)**2*dt**2*Pt(mx_s:mx_e)*(2d0*omega_IR)**2
 
-  zE_shg_n(mx_s:mx_e) = zE_shg_n(mx_s:mx_e) -(2d0*dt*V_SHG)*( zPt(mx_s:mx_e) )
+  E_shg_o(0:nx) = E_shg(0:nx)
+  E_shg(0:nx) = E_shg_n(0:nx)
 
-
-
-  zE_shg_o(0:nx) = zE_shg(0:nx)
-  zE_shg(0:nx) = zE_shg_n(0:nx)
 
 end subroutine dt_evolve
 !-------------------------------------------------------------------------
@@ -142,16 +139,16 @@ subroutine calc_Pt(tt)
   integer :: ix
   real(8) :: ss, st
 
-  zPt = 0d0
+  Pt = 0d0
 
   do ix = mx_s, mx_e
     ss = tt - 0.5d0*Tpulse_IR - xx(ix)/v_IR
     if(abs(ss)<0.5d0*Tpulse_IR)then
-      st = tt - 0.5d0*Tpulse_IR - xx(ix)/v_THz
+      st = tt - 0.5d0*Tpulse_THz - xx(ix)/v_THz
       if(abs(st)<0.5d0*Tpulse_THz)then
-        zPt(ix) = zPt(ix) &
-            -zi* cos(pi*st/Tpulse_THz)**2*cos(omega_THz*st) &
-            *cos(pi*ss/Tpulse_IR)**4*exp(zi*2d0*omega_IR*((1d0/V_SHG)-(1d0/V_IR))*xx(ix))
+        Pt(ix) = Pt(ix) &
+            + cos(pi*st/Tpulse_THz)**2*cos(omega_THz*st) &
+            *cos(pi*ss/Tpulse_IR)**4*cos(2d0*omega_IR*ss)
       end if
     end if
 
@@ -160,21 +157,20 @@ subroutine calc_Pt(tt)
 
 end subroutine calc_Pt
 !-------------------------------------------------------------------------
-subroutine calc_zG_E_shg
+subroutine calc_L_E_shg
   use global_variables
   implicit none
   integer :: ix
 
-  zG_E_shg = 0d0
+  L_E_shg = 0d0
   do ix = 0, nx
-!    zG_E_shg(ix) = ( &
-!        (1d0/12d0)*(zE_shg(ix-2)-zE_shg(ix+2)) &
-!        -(2d0/3d0)*(zE_shg(ix-1)-zE_shg(ix+1)))/hx
-
-    zG_E_shg(ix) = (zE_shg(ix+1)-zE_shg(ix-1))/(2d0*hx)
+    L_E_shg(ix) = ( &
+        -(1d0/12d0)*(E_shg(ix-2)+E_shg(ix+2)) &
+        +(4d0/3d0)*(E_shg(ix-1)+E_shg(ix+1)) &
+        -(5d0/2d0)*E_shg(ix) )/hx**2
   end do
   
-end subroutine calc_zG_E_shg
+end subroutine calc_L_E_shg
 !-------------------------------------------------------------------------
 subroutine write_fields(it)
   use global_variables 
@@ -196,19 +192,19 @@ subroutine write_fields(it)
 
     ss = tt - 0.5d0*Tpulse_IR - xx(ix)/v_IR
     if(abs(ss)<0.5d0*Tpulse_IR)then
-      E_IR = cos(pi*ss/Tpulse_IR)**2 !*cos(omega_IR*ss)
+      E_IR = cos(pi*ss/Tpulse_IR)**2*cos(omega_IR*ss)
     else
       E_IR = 0d0
     end if
 
-    st = tt - 0.5d0*Tpulse_IR - xx(ix)/v_THz
+    st = tt - 0.5d0*Tpulse_THz - xx(ix)/v_THz
     if(abs(st)<0.5d0*Tpulse_THz)then
       E_THz = cos(pi*st/Tpulse_THz)**2*cos(omega_THz*st)
     else
       E_THz = 0d0
     end if
 
-    write(20,"(999e16.6e3)")xx(ix),E_IR,E_THz,abs(zE_shg(ix))
+    write(20,"(999e16.6e3)")xx(ix),E_IR,E_THz,E_shg(ix)
 
   end do
   close(20)
